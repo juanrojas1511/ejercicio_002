0194884c8e2a648ded634ec2443fb030041f74f5
Juan Paris <juanpajares6@gmail.com>
2023-10-04 21:57:23 -0500

    Agregar archivos de proyecto.

---
+++ ejercicio_02/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Divisores de Omar Orlandini que son divisibles por 3:");
+
+            for (int i = 1; i <= 1000; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine($"Divisor de Omar Orlandini {i / 3}: {i}");
+                }
+            }
+
+            
+            Console.ReadKey();
+        }
+    }
+}
